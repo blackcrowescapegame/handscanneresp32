31879da5a468a23791578090736433045cc3b20a
Gabriel Cor Nb26 <gabrielcor@hotmail.com>
2026-09-02 19:56:27 -0400

    upd: secrets updated

---
+++ include/secrets.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define HANDSCANNER_WIFI_SSID "blackcrow_prod01"
+#define HANDSCANNER_WIFI_PASSWORD "e2aVwqCtfc5EsgGE852E"
+#define HANDSCANNER_BEARER_TOKEN "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0MzBhMjJhMmQwMjU0NmZmYWIyOWUxMDA3NWMwMmQ4YyIsImlhdCI6MTczNzIxNTMzNiwiZXhwIjoyMDUyNTc1MzM2fQ.0FDOAfOaIfoYt8N293Ou_bS6ge3bQFmPQyRlUf-CFGs"
+
+// Optional overrides:
+// #define HANDSCANNER_HA_EVENT_URL "http://192.168.70.113:8123/api/events/bc_custom_event"
+// #define HANDSCANNER_RESET_URL "http://192.168.70.113:1880/endpoint/resetHandScanner"
+// #define HANDSCANNER_RESET_POLL_MS 3000UL
